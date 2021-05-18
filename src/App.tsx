6e4a26b3c11a111b7f81f0d0d0feb2d407c5724c
nikola djordjevic <nikola.dj.87@gmail.com>
2021-05-18 21:53:07 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import {useSelector,useDispatch} from 'react-redux'
+import {GetPokemon} from './actions/PokemonActions'
 
+import './App.css';
+import {RootStore} from './Store'
 function App() {
+  const dispatch = useDispatch()
+  const [pokemonName, setPokemonName] = useState('');
+  const pokemonState = useSelector((state:RootStore)=>state.pokemon)
+
+const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setPokemonName(event.target.value);
+
+const handleSubmit = ()=> {
+  dispatch(GetPokemon(pokemonName))
+}
+console.log(pokemonState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <input type="text" onChange={handleChange} />
+  <button onClick={handleSubmit} >Search</button>
+
+
+  {pokemonState.pokemon && (
+    <div>
+      <img src={pokemonState.pokemon.sprites.front_default} alt="" />
+      {pokemonState.pokemon.abilities.map(item=>{
+        return <p key={item.ability.name}>{item.ability.name}</p>
+      })}
+    </div>
+  )}
     </div>
   );
 }
---
+++ src/actions/PokemonActions.ts
@@ -0,0 +1,30 @@
+import { Dispatch } from "redux";
+import {
+  PokemonDispatchTypes,
+  POKEMON_LOADING,
+  POKEMON_FAIL,
+  POKEMON_SUCCESS,
+} from "./PokemonActionTypes";
+
+import axios from "axios";
+
+export const GetPokemon =
+  (pokemon: string) => async (dispatch: Dispatch<PokemonDispatchTypes>) => {
+    try {
+      dispatch({
+        type: POKEMON_LOADING,
+      });
+
+      const res = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+      );
+      dispatch({
+        type: POKEMON_SUCCESS,
+        payload: res.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: POKEMON_FAIL,
+      });
+    }
+  };
